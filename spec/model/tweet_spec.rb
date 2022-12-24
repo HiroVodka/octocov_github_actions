4b563c15623b91d2e4fc78c2dad2d74ae92e02e8
HiroVodka <hiro.vodka06@gmail.com>
2022-12-24 17:51:13 +0900

    add_spec

---
+++ spec/model/tweet_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe 'Tweet', :type => :model do
+  describe 'content' do
+    it do
+      tweet = Tweet.new(content: 'hoge')
+      expect(tweet.valid?).to eq true
+    end
+
+    it do
+      tweet = Tweet.new(content: nil)
+      expect(tweet.valid?).to eq false
+    end
+  end
+end
